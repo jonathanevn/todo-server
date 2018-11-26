0da6701dca82dfbb0a1574281cb3e6cc9f9b065b
Jonathan Eveno <evenojonathan@gmail.com>
2018-11-26 12:25:11 +0100

    Route for create new task created

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 const express = require("express");
 const app = express();
-
+const axios = require("axios");
 const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/To-do-list",
+  { useNewUrlParser: true }
+);
+
 app.use(bodyParser.json());
 
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/to-do-list");
-
 // 1) Definir le model - A faire qu'une fois
-var ToDo = mongoose.model("ToDo", {
-  name: String,
-  age: Number,
-  createdAt: { type: Date, default: Date.now }
+const TaskModel = mongoose.model("ToDo", {
+  title: String,
+  clicked: Boolean
 });
 
-// 2) Créer des documents
-var lucy = new ToDo({
-  name: "Lucy",
-  age: 28
-});
-
-// 3) Sauvegarder des documents
-lucy.save(function(err, obj) {
-  if (err) {
-    console.log("something went wrong");
-  } else {
-    console.log("we just saved your to do" + obj.name);
-  }
-});
-
-// / => hi
+// en GET, pour que le client récupère la liste des tâches
 app.get("/", function(req, res) {
-  res.send("hi");
+  TaskModel.find().exec(function(err, task) {
+    if (!err) {
+      res.json(task);
+    }
+  });
 });
 
-app.get("/hello", function(req, res) {
-  res.send("hello");
+// en POST, pour que le client puisse créer une tâche
+
+app.post("/create", function(req, res) {
+  //je créé un nouveau modèle
+  const newTask = new TaskModel(req.body);
+  console.log("req.body", req.body);
+  console.log("newTask", newTask);
+  //je sauvegarde le modèle
+  newTask.save(function(err, TaskSaved) {
+    console.log("TaskSaved", TaskSaved);
+    if (err) {
+      console.log("err", err);
+      res.json({ error: "erreur" });
+    } else {
+      res.json("We just saved the new task" + TaskSaved);
+    }
+  });
 });
 
-app.get("/bonjour", function(req, res) {
-  res.send("bonjour");
-});
+// en POST, pour que le client puisse rendre une tâche faite ou non-faite
+
+app.post("/update", function(req, res) {});
+
+// en POST, pour que le client puisse supprimer une tâche
+
+app.post("/delete", function(req, res) {});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started");
